408e8cd028d1db21d081c73880965407f060b1f9
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-07-07 17:58:08 +0300

    Edited code to import data from yaml to server

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
-print("This is the server file")
+import yaml
+import os
+
+class Server:
+    def __init__(self, config_path=None):
+        if config_path is None:
+            config_path = os.path.join(os.path.dirname(__file__), '../config.yaml')
+        with open(config_path, 'r') as file:
+            self.config = yaml.safe_load(file)
+        self.server_ip = self.config['ServerIPAddress']
+
+    def run(self):
+        print(f'Current Server IP: {self.server_ip}')
+
+if __name__ == "__main__":
+    client = Server()
+    client.run()
